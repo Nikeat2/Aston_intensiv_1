ef84982023c44271b11b9c8544e50ca26c973d3a
Lobzev Nikita <lobzev.n@yandex.ru>
2024-12-16 00:37:17 +0300

    Финальная версия приложения

--- app/src/main/java/com/example/musicplayeraston/MainActivity.kt
+++ app/src/main/java/com/example/musicplayeraston/MainActivity.kt
@@ -1,20 +1,68 @@
 package com.example.musicplayeraston
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var isPlaying = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnPlay: ImageButton = findViewById(R.id.btnPlay)
+        val btnToNextSong: ImageButton = findViewById(R.id.btnNextSong)
+        val btnToPreviousSong: ImageButton = findViewById(R.id.btnPreviousSong)
+        val btnPause: ImageButton = findViewById(R.id.btnPause)
+
+        btnPlay.setOnClickListener {
+            playMusic()
         }
+
+        btnPause.setOnClickListener {
+            pauseMusic()
+        }
+
+        btnToNextSong.setOnClickListener {
+            nextTrack()
+        }
+
+        btnToPreviousSong.setOnClickListener {
+            previousTrack()
+        }
+
+        val musicServiceIntent = Intent(this, MusicService::class.java)
+        startForegroundService(musicServiceIntent)
+    }
+
+    private fun playMusic() {
+        val playIntent = Intent(this, MusicService::class.java).apply {
+            action = MusicService.ACTION_PLAY
+        }
+        startForegroundService(playIntent)
+        isPlaying = true
+    }
+
+    private fun pauseMusic() {
+        val pauseIntent = Intent(this, MusicService::class.java).apply {
+            action = MusicService.ACTION_PAUSE
+        }
+        startForegroundService(pauseIntent)
+        isPlaying = false
+    }
+
+    private fun nextTrack() {
+        val nextIntent = Intent(this, MusicService::class.java).apply {
+            action = MusicService.ACTION_NEXT
+        }
+        startForegroundService(nextIntent)
+    }
+
+    private fun previousTrack() {
+        val previousIntent = Intent(this, MusicService::class.java).apply {
+            action = MusicService.ACTION_PREVIOUS
+        }
+        startForegroundService(previousIntent)
     }
 }
